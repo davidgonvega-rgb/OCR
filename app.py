400b8477476892215590aa3f0de4858e3c2974bb
David Gonzalez Vega <david.gon.vega@gmail.com>
2026-07-23 13:07:48 -0600

    Improve OCR extraction for El Salvador bank receipts

--- app.py
+++ app.py
@@ -1773,15 +1773,7 @@ with form_column:
         key=date_widget_key,
     )
 
-    if uploaded_file is None:
-        st.html(
-            """
-<div class="required-message">
-    Este documento es obligatorio
-</div>
-"""
-        )
-
+ 
     parsed_amount = parse_amount(amount)
 
     amount_is_valid = (
